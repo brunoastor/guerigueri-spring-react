fef7cb64fc3b4e3547df0b84f456afc65cdb36ce
bruno <bruno.ams@hotmail.com>
2023-09-20 09:48:58 -0300

    front

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
+import React, {useState} from "react";
+
+function App() {
+
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+
+  return (
+    <div>
+      <Formulario botao={btnCadastrar} />
+      <Tabela />
+    </div>
+  );
+}
+
+export default App;
